cd5e7118b9361db9a15e00f3b23092e55b2c7698
Marlene Carli <maarlen.car@hotmail.com>
2022-07-01 17:06:40 -0300

    Static Site

--- 07-webserver/app.js
+++ 07-webserver/app.js
@@ -5,15 +5,18 @@ const port = 8081;
 
 //Servir Contenido estatico 
 
-app.use( express.static('templated-roadtrip') )
+app.use( express.static('public') )
 
-app.get('/', (req, res) => {
-  res.send('Home Page')
+app.get('/generic', (req, res) => {
+res.sendFile(__dirname +'/public/generic.html')
 })
-
+app.get('/elements', (req, res) => {
+res.sendFile(__dirname +'/public/elements.html')
+})
 app.get('*', (req, res) => {
 res.sendFile(__dirname +'/public/404.html')
 })
+
 
 app.listen(port, () => {
     console.log(`Running on http://localhost:${port}`)
