e92869aaec61434981c0df9927fe489c225e85d7
Marlene Carli <maarlen.car@hotmail.com>
2022-07-02 15:27:46 -0300

    Fin Web Server

--- 07-webserver/app.js
+++ 07-webserver/app.js
@@ -1,17 +1,38 @@
 const express = require('express')
-const app = express()
+const hbs = require('hbs');
+require('dotenv').config();
 
-const port = 8081;
+const app = express()
+const port = process.env.PORT;
+
+
+// Handlebars
+app.set('view engine', 'hbs');
+
+hbs.registerPartials(__dirname + '/views/partials', (err) => {
+    console.log(err);
+})
 
 //Servir Contenido estatico 
-
 app.use( express.static('public') )
 
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'Marlene',
+        titulo: 'Proyecto Web Server'
+    })
+})
 app.get('/generic', (req, res) => {
-res.sendFile(__dirname +'/public/generic.html')
+    res.render('generic', {
+        nombre: 'Marlene',
+        titulo: 'Proyecto Web Server'
+    })
 })
 app.get('/elements', (req, res) => {
-res.sendFile(__dirname +'/public/elements.html')
+    res.render('elements', {
+        nombre: 'Marlene',
+        titulo: 'Proyecto Web Server'
+    })
 })
 app.get('*', (req, res) => {
 res.sendFile(__dirname +'/public/404.html')
